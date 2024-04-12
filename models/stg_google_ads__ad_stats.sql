1a250170f340fef97a58aa6f75676b19ee46b385
nam-97 <nam.nx@joonsolutions.com>
2024-04-12 18:00:23 +0700

    add conversions

--- models/stg_google_ads__ad_stats.sql
+++ models/stg_google_ads__ad_stats.sql
@@ -10,13 +10,7 @@ with base as (
 fields as (
 
     select
-        {{
-            fivetran_utils.fill_staging_columns(
-                source_columns=adapter.get_columns_in_relation(ref('stg_google_ads__ad_stats_tmp')),
-                staging_columns=get_ad_stats_columns()
-            )
-        }}
-        
+        *        
     
         {{ fivetran_utils.source_relation(
             union_schema_variable='google_ads_union_schemas', 
@@ -44,7 +38,8 @@ final as (
         campaign_id, 
         clicks, 
         cost_micros / 1000000.0 as spend, 
-        impressions
+        impressions,
+        conversions
         
         {{ fivetran_utils.fill_pass_through_columns('google_ads__ad_stats_passthrough_metrics') }}
 
